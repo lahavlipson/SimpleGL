2fc2f93ee6a845471fe537c4a4c085774f3d6554
Yuxuan Mei <yuxuan.mei@icloud.com>
2019-04-09 13:20:31 -0400

    reformat dir structure

--- build/CMakeFiles/3.10.3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/lahavlipson/Personal\ Projects/Learn_OpenGL/build/CMakeFiles/3.10.3/CompilerIdC/CMakeCCompilerId.c
--- build/GLFW.build/Debug/ALL_BUILD.build/Script-A9CCB427343747C18298D0A4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/lahavlipson/Personal\ Projects/Learn_OpenGL/build -f /Users/lahavlipson/Personal\ Projects/Learn_OpenGL/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
