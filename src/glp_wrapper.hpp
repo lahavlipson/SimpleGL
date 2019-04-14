a9faa8cf8a932822332492b93634f684bb15269e
Yuxuan Mei <merlinyx@users.noreply.github.com>
2019-04-14 12:57:54 -0400

    Update glp_wrapper.hpp

--- src/glp_wrapper.hpp
+++ src/glp_wrapper.hpp
@@ -1,22 +1,25 @@
 #ifndef GLP_WRAPPER_HPP
 #define GLP_WRAPPER_HPP
 
-#include <variant>
 #include <glp/glp.h>
 #include <obj_loader/obj_loader.h>
 
+#include <variant>
+
 enum Shape { custom, sphere, truncatedCone, cylinder, cone, pyramid, torus, box, obj };
 
-inline std::vector<double> createGLPObj(Shape s, std::variant<std::vector<double>, std::string> varP, bool isDefault = true) {
+inline std::vector<double> createGLPObj(
+    Shape s, std::variant<std::vector<double>, std::string> varP, bool isDefault = true) {
+    
 	std::vector<double> p;
 	std::string filepath;
-	
-	if(std::holds_alternative<std::vector<double>>(varP))
+	if (std::holds_alternative<std::vector<double>>(varP)) {
 		p = std::get<std::vector<double>>(varP);
-	else
+	} else {
 		filepath = std::get<std::string>(varP);
+	}
 
-	switch(s) {
+	switch (s) {
 	case custom:
 		// p is the vertices for Shape::custom, directly return
 		return p;
@@ -57,27 +60,27 @@ inline std::vector<double> createGLPObj(Shape s, std::variant<std::vector<double
 		}
 		return glp::torus((int) p[0], (int) p[1], p[2], p[3], glm::dvec3(p[4],p[5],p[6]));
 
-    case obj: {
-        objl::Loader loader;
-        loader.LoadFile(filepath);
-        std::vector<double> output;
-        for (int i=0; i<loader.LoadedVertices.size(); i++){
-            output.push_back(loader.LoadedVertices[i].Position.X);
-            output.push_back(loader.LoadedVertices[i].Position.Y);
-            output.push_back(loader.LoadedVertices[i].Position.Z);
-            output.push_back(loader.LoadedVertices[i].Normal.X);
-            output.push_back(loader.LoadedVertices[i].Normal.Y);
-            output.push_back(loader.LoadedVertices[i].Normal.Z);
-        }
-        assert(output.size() == 6*loader.LoadedVertices.size());
-        return output;
-    }
-
 	case box:
 		if (isDefault) {
 			return glp::box(glm::dvec3(p[0],p[1],p[2]));
 		}
 		return glp::box(glm::dvec3(p[0],p[1],p[2]), glm::dvec3(p[3],p[4],p[5]));
+	}
+
+	case obj: {
+		objl::Loader loader;
+		loader.LoadFile(filepath);
+		std::vector<double> output;
+		for (int i = 0; i < loader.LoadedVertices.size(); i++){
+			output.push_back(loader.LoadedVertices[i].Position.X);
+			output.push_back(loader.LoadedVertices[i].Position.Y);
+			output.push_back(loader.LoadedVertices[i].Position.Z);
+			output.push_back(loader.LoadedVertices[i].Normal.X);
+			output.push_back(loader.LoadedVertices[i].Normal.Y);
+			output.push_back(loader.LoadedVertices[i].Normal.Z);
+		}
+		assert (output.size() == 6 * loader.LoadedVertices.size());
+		return output;
 	}
 }
 
