13402f6ce6ee66a1d461f9c5667a5d70e296a576
Crystal Ren <crystalren97@gmail.com>
2019-04-21 18:24:28 -0400

    fixed view

--- test/src/compare2.cpp
+++ test/src/compare2.cpp
@@ -19,7 +19,7 @@ void checkCompileErrors(GLuint shader, std::string type);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 // camera
 glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f, 3.0f);
