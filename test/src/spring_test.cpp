edf4e15637c4bbe6fd6f1a61a672f646e3f9a103
Lahav174 <lahav.lipson@columbia.edu>
2019-04-21 13:07:37 -0400

    Improved spring physics. Integrated new scene methods into sprint_test.

--- test/src/spring_test.cpp
+++ test/src/spring_test.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 
-#include "glp_wrapper.hpp"
 #include "scene.hpp"
 #include <thread>
 #include <unistd.h>
@@ -13,34 +12,34 @@ class Spring {
 
 public:
 
-    Spring(mesh_id a, mesh_id b, mesh_id s, double restL, double constant):id1(a),id2(b),k(constant), rest(restL), obj_id(s) {}
+    Spring(Mesh_id a, Mesh_id b, Mesh_id s, double restL, double constant):id1(a),id2(b),k(constant), rest(restL), obj_id(s) {}
 
-    mesh_id id1;
-    mesh_id id2;
-    mesh_id obj_id;
+    Mesh_id id1;
+    Mesh_id id2;
+    Mesh_id obj_id;
     const float k;
     const float rest;
 
-    inline glm::vec3 getP1(Scene *s){
-        return s->get_loc(id1);
+    inline glm::vec3 getP1(){
+        return id1.get_loc();
     }
     
-    inline glm::vec3 getP2(Scene *s){
-        return s->get_loc(id2);
+    inline glm::vec3 getP2(){
+        return id2.get_loc();
     }
     
-    inline float length(Scene *s){
-        const glm::vec3 p1 = getP1(s);
-        const glm::vec3 p2 = getP2(s);
+    inline float length(){
+        const glm::vec3 p1 = getP1();
+        const glm::vec3 p2 = getP2();
         return glm::distance(p1, p2);
     }
     
-    inline glm::vec3 dir(Scene *s){
-        return glm::normalize(getP2(s) - getP1(s));
+    inline glm::vec3 dir(){
+        return glm::normalize(getP2() - getP1());
     }
 
-    inline glm::vec3 force(Scene *s){
-        return k*(rest - length(s))*dir(s);
+    inline glm::vec3 force(){
+        return k*(rest - length())*dir();
     }
 
 };
@@ -51,43 +50,42 @@ public:
     
     bool stopSim = false;
     
-    Scene *s = nullptr;
-    
     inline void cancel(){
         stopSim = true;
     }
     
-    Simulation(Scene *scn):s(scn){}
+    Simulation(){}
     
     
     inline void physics(Spring spr)
     {
         
-        s->reset_model(spr.obj_id);
-        s->translate(spr.obj_id, glm::vec3(0, 0, -3));
-        s->scale(spr.obj_id, {0.4, 0.6, 0.4});
+        spr.obj_id.reset_model();
+        spr.obj_id.translate(glm::vec3(0, 0, -3));
+        spr.obj_id.scale({0.4, 0.6, 0.4});
         
-        s->rotate(spr.obj_id, 0, glm::vec3(-1,0,0));
+        spr.obj_id.rotate(0, glm::vec3(-1,0,0));
         const float t = 0.1f;
-        glm::vec3 v1 = {0, 0, -0.3};
+        glm::vec3 v1 = {0.3, 0, -0.3};
         while (!stopSim){
             
-            double ball1Height = spr.getP2(s) [1];
-            const glm::vec3 oldDir = spr.dir(s);
-            glm::vec3 F1 = spr.force(s);
+            double ball1Height = spr.length();
+            glm::vec3 F1 = spr.force() - glm::vec3(0,0.3,0);
 
             v1 += F1*t;
 
-            s->translate(spr.id2, v1*t);
+            spr.id2.translate(v1*t);
             
-            double newBall1Height = spr.getP2(s)[1];
-            glm::vec3 crossProd = glm::cross(glm::vec3(0,1,0.000001), spr.dir(s));
+            double newBall1Height = spr.length();
+            glm::vec3 crossProd = glm::cross(glm::vec3(0,1,0.000001), spr.dir());
             
-            float angleDiff = glm::degrees(float(acos(glm::dot(spr.dir(s), oldDir))));
-            s->rotate(spr.obj_id, angleDiff, glm::vec3(-1,0,0));
+            float angleDiff = glm::degrees(float(acos(glm::dot(spr.dir(), {0,1,0}))));
+            spr.obj_id.set_rotation(angleDiff, crossProd);
             
-            //s->scale(spr.obj_id, {1,ball1Height/newBall1Height,1});
-            usleep(10000);
+           // std::cout << glm::degrees(angleDiff) << "\n";
+            
+            spr.obj_id.scale({1,newBall1Height/ball1Height,1});
+            usleep(12000);
             
         }
     }
@@ -104,20 +102,20 @@ int main(int argc, char *argv[]){
     
     // add two spheres
     color col = glm::vec3(0.7, 0.5, 0.5);
-    mesh_id id_1 = s.add_mesh(Shape::sphere, col );
-    s.translate(id_1, glm::vec3(0, 0, -3));
-    s.scale(id_1, 0.5);
-    mesh_id id_2 = s.add_mesh(Shape::sphere, col);
-    s.translate(id_2, glm::vec3(0,3.5,-3));
-    s.scale(id_2, 0.5);
+    Mesh_id id_1 = s.add_mesh(Shape::sphere, col );
+    id_1.translate(glm::vec3(0, 0, -3));
+    id_1.scale(0.5);
+    Mesh_id id_2 = s.add_mesh(Shape::sphere, col);
+    id_2.translate(glm::vec3(0,3.5,-3));
+    id_2.scale(0.5);
     
-    mesh_id spring_id = s.add_mesh(Shape::obj, {0.4, 0.4, 0.4}, argv[1]);
+    Mesh_id spring_id = s.add_mesh("object1", {0.4, 0.4, 0.4}, argv[1]);
     
     
     
-    Simulation sim(&s);
+    Simulation sim;
     
-    std::vector<mesh_id> ids = {id_1, id_2, spring_id};
+    std::vector<Mesh_id> ids = {id_1, id_2, spring_id};
     Spring spring(id_1, id_2, spring_id, 4.0, 2.0);
     
     std::thread t1(&Simulation::physics, &sim, spring);
