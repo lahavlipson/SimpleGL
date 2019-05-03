3e3be1be8b5bf56d50f78351e1671cca6d003f7a
Yuxuan Mei <yuxuan.mei@icloud.com>
2019-05-03 00:54:38 -0400

    separate adding and transforming objects for the slides

--- test/src/test1.cpp
+++ test/src/test1.cpp
@@ -21,36 +21,36 @@ int main(int argc, char *argv[]) {
         s.set_shadow(true);
         s.set_light_pos({30,30,-30});
         
+        ObjId floor = s.add_obj(Shape::box, {0.6, 0.6, 0.6});
         const double radius = 5;
         for (int i=0; i<10; i++){
+            ObjId sphere = s.add_obj(Shape::sphere, RED);
+            ObjId truncated_cone = s.add_obj(Shape::truncatedCone, BLUE);
+            ObjId cylinder = s.add_obj(Shape::cylinder, GREEN);
+            ObjId cone = s.add_obj(Shape::cone, YELLOW);
+            ObjId pyramid = s.add_obj(Shape::pyramid, PINK);
+            ObjId torus = s.add_obj(Shape::torus, GREEN, {.r1 = 0.7, .r2 = 0.4, .accuracy = 5});
+            ObjId box = s.add_obj(Shape::box, TEAL);
+            ObjId kitten = s.add_obj("kitten", ORANGE, {.filepath = "./test/obj_files/kitten.obj"});
+            
             double angle = (M_PI/4)*(1+i);
-            ObjId id = s.add_obj(Shape::sphere, RED);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            sphere.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(2+i);
-            id = s.add_obj(Shape::truncatedCone, BLUE);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            truncated_cone.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(3+i);
-            id = s.add_obj(Shape::cylinder, GREEN);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            cylinder.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(4+i);
-            id = s.add_obj(Shape::cone, YELLOW);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            cone.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(5+i);
-            id = s.add_obj(Shape::pyramid, PINK);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            pyramid.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(6+i);
-            id = s.add_obj(Shape::torus, GREEN, {.r1 = 0.7, .r2 = 0.4, .accuracy=5});
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            torus.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(7+i);
-            id = s.add_obj(Shape::box, TEAL);
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            box.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
             angle = (M_PI/4)*(8+i);
-            id = s.add_obj("cat", ORANGE, {.filepath = "./test/obj_files/kitten.obj"});
-            id.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
-            id.scale(2);
+            kitten.translate({radius*cos(angle),radius*sin(angle), -5 - i*6});
+            kitten.scale(2);
         }
-        
-        ObjId floor = s.add_obj(Shape::box, {0.6, 0.6, 0.6} );
         floor.translate(glm::vec3(-35, -7, -35));
         floor.scale({70, 0.01, 70});
         
